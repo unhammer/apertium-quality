3127c0df6de071c35e098b2b9e7d71508650ed48
Brendan Molloy <brendan@bbqsrc.net>
2011-05-24 14:54:30 +1000

    Let's try this...

---
+++ apertium_quality/core.py
@@ -0,0 +1,62 @@
+import xml.etree.cElementTree as etree
+from xml.etree.cElementTree import ParseError
+import os, os.path
+from hashlib import sha1
+from datetime import datetime
+#import logging
+
+def checksum(data):
+	return str(sha1(data).hexdigest())
+
+def whereis(program):
+	for path in os.environ.get('PATH', '').split(':'):
+		if os.path.exists(os.path.join(path, program)) and \
+		   not os.path.isdir(os.path.join(path, program)):
+			return os.path.join(path, program)
+	return None
+
+class Statistics(object):
+	file_version = "1.0"
+
+	def __init__(self, f):
+		self.f = f
+		if os.path.exists(f):
+			try:
+				self.tree = etree.parse(open(f))
+				if self.tree.getroot().tag == "statistics":
+					if self.tree.getroot().get('version') == "1.0":	
+						print "[STUB] Do version specific crap here for 1.0"
+					else:
+						print "[DEBUG] Version incorrect."
+					self.root = self.tree.getroot()
+					print "[DEBUG] Imported tree."
+				else:
+					raise ParseError("File does not seem to be a statistics file.")
+			except IOError:
+				raise
+			except ParseError:
+				raise
+			except:
+				raise
+		else:
+			xml = '<statistics type="%s" version="%s" />' % ("apertium", Statistics.file_version)
+			try:
+				self.root = etree.fromstring(xml)
+				self.tree = etree.ElementTree(self.root)
+			except:
+				raise
+	
+	def write(self):
+		self.tree.write(self.f, encoding="utf-8", xml_declaration=True)
+
+	def add_regression(self, name, cksum, passes, fails):
+		root = self.root.find('regressions')
+		r = etree.SubElement(root, 'regression', date=datetime.now().isoformat())
+		etree.SubElement(r, 'name').text = str(name)
+		etree.SubElement(r, 'checksum', type='sha1').text = str(cksum)
+		etree.SubElement(r, 'passes').text = str(passes)
+		etree.SubElement(r, 'fails').text = str(fails)
+		
+
+	
+		
---
+++ apertium_quality/regression_testing.py
@@ -0,0 +1,64 @@
+import xml.etree.cElementTree as etree
+from cStringIO import StringIO
+from apertium_quality.core import whereis
+from collections import defaultdict
+from subprocess import * 
+import urllib
+
+class RegressionTest(object):
+	ns = "{http://www.mediawiki.org/xml/export-0.3/}"
+	program = "apertium"
+	
+	def __init__(self, url, mode, directory="."):
+		if not whereis(self.program):
+			raise IOError("Cannot find `%s`. Check $PATH." % self.program)	
+		if not "Special:Export" in url:
+			raise AttributeError("URL did not contain Special:Export.")
+		self.mode = mode
+		self.directory = directory
+		self.tree = etree.parse(urllib.urlopen(url))
+		self.passes = 0
+		self.total = 0
+		for e in self.tree.getroot().getiterator():
+			if e.tag == self.ns + "title":
+				self.title = e.text
+			if e.tag == self.ns + "revision":
+				self.revision = e[0].text # should be <id>
+			if e.tag == self.ns + "text":
+				self.text = e.text
+		if not self.text:
+			raise AttributeError("No text element?")
+		self.tests = defaultdict(defaultdict)
+		for i in self.text.split('\n'):
+			if i[:4] == "* {{":
+				x = i.strip("{}* ").split('|')
+				self.tests[x[1]][x[2]] = x[3]
+		self.out = StringIO()
+	
+	def run(self):
+		for side in self.tests:
+			self.out.write("Now testing: %s\n" % side)
+			
+			app = Popen([self.program, '-d', self.directory, self.mode],
+				stdin=PIPE, stdout=PIPE, stderr=PIPE)
+			args = '\n'.join(self.tests[side].keys()) + '\n'
+			app.stdin.write(args.encode('utf-8'))
+			self.results = app.communicate()[0].decode('utf-8').split('\n')
+			
+			for n, test in enumerate(self.tests[side].items()):
+				self.out.write("%s\t  %s\n" % (self.mode, test[0].encode('utf-8')))
+				print self.results[n].strip(), test
+				if self.results[n].strip() == test[1].strip():
+					self.out.write("WORKS\t  %s\n" % self.results[n].encode('utf-8'))
+					self.passes += 1
+				else:
+					self.out.write("\t- %s\n" % test[1].encode('utf-8'))
+					self.out.write("\t+ %s\n" % self.results[n].encode('utf-8'))
+				self.total += 1
+
+	def start(self):
+		self.run()
+
+	def get_output(self):
+		print self.out.getvalue()
+		print "%d/%d %d%" % (self.passes, self.total, self.passes / self.total * 100)
