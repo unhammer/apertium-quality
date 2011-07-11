03b076de0264c9abd0cad1a3a60cf65c9e7cb970
Brendan Molloy <brendan@bbqsrc.net>
2011-07-11 18:48:44 +1000

    Derp

--- apertium/quality/__init__.py
+++ apertium/quality/__init__.py
@@ -482,20 +482,20 @@ chronodiv = """
 				<h1>Chronological Statistics</h1>
 				<ul>
 				% for date in reversed(chrono_stats):
-					% print(date, chrono_stats[date])
-					<li>
-						<a href="#" id="${date}">${date}</a>
-						<div id="${date}-div">
-							<table>
-							% for k, v in chrono_stats[date].items():
-								<tr>
-									<td>${k}</td>
-									<td>${v}</td>
-								</tr>
-							% endfor
-							</table>
-						</div>
-					</li>
+					${date} ${chrono_stats[date]} <br />
+					##<li>
+					##	<a href="#" id="${date}">${date}</a>
+					##	<div id="${date}-div">
+					##		<table>
+					##		% for k, v in chrono_stats[date].items():
+					##			<tr>
+					##				<td>${k}</td>
+					##				<td>${v}</td>
+					##			</tr>
+					##		% endfor
+					##		</table>
+					##	</div>
+					##</li>
 				% endfor
 				</ul>
 			</div>
