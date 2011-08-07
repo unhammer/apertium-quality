8f1244c8f4965e094ea60eaeb912e4f7721483d6
Brendan Molloy <brendan@bbqsrc.net>
2011-08-07 18:22:54 +1000

    Frontend update

--- apertium/quality/frontend/dictionary_tester.py
+++ apertium/quality/frontend/dictionary_tester.py
@@ -5,9 +5,12 @@ class UI(Frontend, DictionaryTest):
     def __init__(self):
         Frontend.__init__(self)
         self.description = "Get general dictionary statistics."
-        self.add_argument("dictionary", nargs=1, help="Dictionary file (.dix)")
+        self.add_argument("-d", "--dict", dest="dictdir", nargs='?',
+            const=['.'], default=['.'],
+            help="Directory of dictionary (Default: current directory)")
+        self.add_argument("langpair", nargs=1, help="Language pair (eg aa-ab)")
         self.args = self.parse_args()
-        DictionaryTest.__init__(self, self.args.dictionary[0])
+        DictionaryTest.__init__(self, self.args.langpair[0], self.args.directory[0])
 
 def main():
     try:
