6edc338c7c9385fad40e3618aab65e45d52d5528
Brendan Molloy <brendan@bbqsrc.net>
2011-08-14 22:54:11 +1000

    new things

--- apertium/quality/frontend/auto_tester.py
+++ apertium/quality/frontend/auto_tester.py
@@ -7,13 +7,14 @@ class UI(Frontend, AutoTest):
 		Frontend.__init__(self, stats=False, colour=True)
 		self.description="Attempt all tests with default settings."
 		self.add_argument("-X", "--statistics", dest="stats", 
-                          nargs='?', const='quality-stats.xml', default=None,
+                          nargs='?', const=['quality-stats.xml'], default=[None],
                           help="XML file that statistics are to be stored in")
-		self.add_argument("-H", "--html", dest="outdir", nargs=1, 
+		self.add_argument("-H", "--html", dest="outdir", nargs='?',
+						  const=[None], default=['htmlout'], 
       					  help="Output directory for HTML content")
 		self.add_argument("aqx", nargs=1, help="Apertium Quality XML configuration file")
 		self.args = args = self.parse_args()
-		AutoTest.__init__(self, self.args.stats, self.args.outdir[0], self.args.aqx[0])
+		AutoTest.__init__(self, self.args.stats[0], self.args.outdir[0], self.args.aqx[0])
 		
 
 def main():
