33cc70050223bece76758d3b42f99b01f6fedfd3
Brendan Molloy <brendan@bbqsrc.net>
2011-08-22 11:03:13 +1000

    Voctest frontend bugfix

--- apertium/quality/frontend/vocabulary_tester.py
+++ apertium/quality/frontend/vocabulary_tester.py
@@ -6,26 +6,26 @@ class UI(Frontend, VocabularyTest):
         Frontend.__init__(self)
         self.description = "Test vocabulary for generation errors."
         self.add_argument("-a", "--anadix", dest="anadix", nargs='?',
-            const=[None], default=[None],
+            const=None, default=None,
             help="Analysis .dix file (Default: apertium-aa-ab.aa-ab.dix)")
         self.add_argument("-g", "--genbin", dest="genbin", nargs='?',
-            const=[None], default=[None],
+            const=None, default=None,
             help="Generation .bin file (Default: apertium-aa-ab.autogen.bin)")
         self.add_argument("-d", "--dict", dest="dictdir", nargs='?',
-            const=['.'], default=['.'],
+            const='.', default='.',
             help="Directory of dictionary (Default: current directory)")
         self.add_argument("-D", "--direction", dest="direction", nargs='?',
-            const=['lr'], default=['lr'],
+            const='lr', default='lr',
             help="Dictionary direction (lr, rl)")
         self.add_argument("-o", "--output", dest="output", nargs='?',
-            const=['voctest.txt'], default=['voctest.txt'],
+            const='voctest.txt', default='voctest.txt',
             help="Output file for arrows output")
         self.add_argument("langpair", nargs=1, help="Language pair (eg, br-fr)")
         self.args = self.parse_args()
         lang1, lang2 = self.args.langpair[0].split('-')
-        VocabularyTest.__init__(self, self.args.direction[0], lang1, lang2, 
-                                self.args.output[0], self.args.dictdir[0],
-                                self.args.anadix[0], self.args.genbin[0])
+        VocabularyTest.__init__(self, self.args.direction, lang1, lang2, 
+                                self.args.output, self.args.dictdir,
+                                self.args.anadix, self.args.genbin)
         
 def main():
     try:
