4a4bdc10e14a6717459a626fb1682de9bf4475f5
Brendan Molloy <brendan@bbqsrc.net>
2011-08-22 11:47:16 +1000

    A broken tab.

--- apertium/quality/frontend/__init__.py
+++ apertium/quality/frontend/__init__.py
@@ -5,25 +5,25 @@ from apertium.quality import Statistics, ParseError
 #TODO add piping for great interfacing
 
 class Frontend(Test, ArgumentParser):
-    def __init__(self, stats=True, colour=False):
-        ArgumentParser.__init__(self)
-        Test.__init__(self)
-        if colour:
-            self.add_argument("-c", "--colour", dest="colour", action="store_true",
-                              help="Colours the output") 
-        if stats:
-            self.add_argument("-X", "--statistics", dest="statfile", 
-                              nargs='?', const='quality-stats.xml', default=None,
-                              help="XML file that statistics are to be stored in")
+	def __init__(self, stats=True, colour=False):
+		ArgumentParser.__init__(self)
+		Test.__init__(self)
+		if colour:
+			self.add_argument("-c", "--colour", dest="colour", action="store_true",
+							  help="Colours the output") 
+		if stats:
+			self.add_argument("-X", "--statistics", dest="statfile", 
+							  nargs='?', const='quality-stats.xml', default=None,
+							  help="XML file that statistics are to be stored in")
 
-    def start(self):
-        ret = self.run()
-        print(self.to_string())
-        if self.args.statfile:
-            try:
+	def start(self):
+		ret = self.run()
+		print(self.to_string())
+		if self.args.statfile:
+			try:
 				stats = Statistics(self.args.statfile)
 				stats.add(*self.to_xml())
 				stats.write()
 			except ParseError:
 				print("ERROR: your statistics file is either an unsupported version or not an XML file.")
-        self.exit(ret)
+		self.exit(ret)
